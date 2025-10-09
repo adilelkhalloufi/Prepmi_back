008db0b26d4807a7ed3a2692bf5ceb714fcc859e
adil elkhalloufi <adilelkhalloufi@gmail.com>
2025-10-09 15:29:41 +0100

    Add config route for debug mode with database refresh and seeding

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,15 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BidController;
-use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\FavarisController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SpecialitieController;
-use App\Http\Controllers\UniteController;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 //  Public routes
-Route::get('categorie', [CategorieController::class, 'index']);
-Route::get('unites', [UniteController::class, 'index']);
-Route::get('products', [ProductController::class, 'GetProduct']);
-Route::get('specialities', [SpecialitieController::class, 'index']);
 
 Route::post('login', [AuthController::class, 'login']);
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('forget-password', [AuthController::class, 'forgetPassword']);
 
@@ -26,7 +18,7 @@ Route::post('forget-password', [AuthController::class, 'forgetPassword']);
 Route::group(['middleware' => ['auth:sanctum']], function (): void {
 
     Route::post('logout', [AuthController::class, 'logout']);
-    
+
     Route::get('GetOrderForSeller', [OrderController::class, 'GetOrderForSeller']);
     Route::resource('product', ProductController::class);
     Route::resource('favoris', FavarisController::class);
@@ -41,12 +33,9 @@ Route::group(['middleware' => ['auth:sanctum']], function (): void {
 Route::get('config', function () {
     // test if app in debug mode
 
-    if(env('APP_DEBUG') == true){
+    if (env('APP_DEBUG') == true) {
 
         Artisan::call('migrate:refresh');
         Artisan::call('db:seed');
-
     }
-       
-
 });
